41eb9bda48060633709e27e0daec88fde526598d
Trevor Plassman <tplassman@wrayward.com>
2019-09-24 16:53:57 -0400

    removing utilities files and using npm packages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 import './polyfills';
+import pop from 'compop';
 
 // Components
 // import Header from './components/header';
 
-// Utilities
-import { instantiate } from './utilities/components';
+const { SITE_HANDLE } = process.env;
 
 /* eslint-disable quote-props */
 const classMap = {
@@ -12,10 +12,26 @@ const classMap = {
 };
 /* eslint-enable quote-props */
 
+const actions = {
+	// Action events
+	lockScroll: 'lock-scroll',
+	unlockScroll: 'unlock-scroll',
+	showHeader: 'show-header',
+	hideHeader: 'hide-header',
+	closeHeader: 'close-header',
+};
+
+
 // Event handler functions
 function handleDOMConentLoaded() {
-	// Call component constructors
-	instantiate(classMap);
+    const scaffold = window[SITE_HANDLE];
+
+    function cb() {
+        // Do something after components initialize
+    }
+
+    // Call component constructors
+    pop({ scaffold, classMap, actions, cb });
 }
 
 // Add event listeners
